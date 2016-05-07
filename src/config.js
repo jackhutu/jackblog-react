d7ee87f3084bb55f7a5d8754213901c6d576466b
Jack Hu <hutufeia2009@sina.com>
2016-05-07 13:02:38 +0800

    update to v2.1.2

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const API_ROOT = (process.env.NODE_ENV === 'production')
 		? 'http://api.jackhu.top/'
-		:'http://localhost:9000'
+		:'http://localhost:9000/'
 
 export const CookieDomain = (process.env.NODE_ENV === 'production')
 		? '.jackhu.top'
